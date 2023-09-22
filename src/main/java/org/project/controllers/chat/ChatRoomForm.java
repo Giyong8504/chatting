edc0197f7e391b2670d87b413e69b1ac76c656ba
Giyong8504 <kky5163@naver.com>
2023-09-22 21:50:03 +0900

    채팅 기록 구현중

--- src/main/java/org/project/controllers/chat/ChatRoomForm.java
+++ src/main/java/org/project/controllers/chat/ChatRoomForm.java
@@ -1,7 +1,12 @@
 package org.project.controllers.chat;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 // 데이터를 담기 위한 용도
+// getter, 생성사, toString, equalsAndHashCode 자동 추가.
 @Builder
-public record ChatRoomForm(Long roomNo, String roomNm, int max) {}// getter, 생성사, toString, equalsAndHashCode 자동 추가.
+public record ChatRoomForm(
+        Long roomNo,
+        @NotBlank String roomNm,
+        int max) {}
